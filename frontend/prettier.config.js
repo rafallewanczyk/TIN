658640ae477e6050b27eadf9f393a10a2e42f0dd
kostkabrukowa <jiirra@gmail.com>
2020-04-15 13:38:40 +0200

    TIN-34 | Added starter project for frontend

---
+++ frontend/prettier.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  tabWidth: 2,
+  tabs: false,
+  printWidth: 100,
+  semi: true,
+  trailingComma: 'all',
+  singleQuote: true,
+  quoteProps: 'as-needed',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: 'always',
+};
