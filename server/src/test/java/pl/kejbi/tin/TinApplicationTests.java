0072066f3c3462765e5c2b83bd34068eb9406831
kejbi <biegajski.kacper@gmail.com>
2020-04-24 21:15:58 +0200

    TIN-45 | project set up

---
+++ server/src/test/java/pl/kejbi/tin/TinApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kejbi.tin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
