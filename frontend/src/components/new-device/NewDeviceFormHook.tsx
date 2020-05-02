2ae76dfd8f8cc33c392851da27921660d3d8a484
kostkabrukowa <jiirra@gmail.com>
2020-05-02 15:08:28 +0200

    Fetches regulators in new device form instead of mock data

--- frontend/src/components/new-device/NewDeviceFormHook.tsx
+++ frontend/src/components/new-device/NewDeviceFormHook.tsx
@@ -1,16 +1,15 @@
 import { Form } from 'antd';
 import { FormInstance } from 'antd/es/form';
-import { queryCache, useMutation } from 'react-query';
+import { queryCache, useMutation, useQuery } from 'react-query';
 import { useNavigate } from '@reach/router';
-import {
-  DeviceModel,
-  DeviceType,
-  RegulatorModel,
-  Status,
-} from '../models/regulator-device-model/RegulatorDeviceModel';
+import { DeviceModel, RegulatorModel } from '../models/regulator-device-model/RegulatorDeviceModel';
 import { addNewDevice, editDeviceWithId } from '../rest-client/devices/DevicesRestClient';
-import { ALL_DEVICES_QUERY } from '../all-devices-list/devices-list/useDevicesQuery';
+import {
+  ALL_DEVICES_QUERY,
+  ALL_REGULATORS_QUERY,
+} from '../all-devices-list/devices-list/useDevicesQuery';
 import { encodeInBase64 } from '../utils/form/PublicKeyUtils';
+import { fetchRegulators } from '../rest-client/devices/RegulatorsRestClient';
 
 export enum NewDeviceFieldNames {
   name = 'name',
@@ -37,8 +36,11 @@ export const useNewDeviceForm: (
 } = (device) => {
   const [form] = Form.useForm();
   const navigate = useNavigate();
+  const { data: regulators } = useQuery(ALL_REGULATORS_QUERY, fetchRegulators, {
+    suspense: true,
+  });
   const initialValues = device && createInitialValues(device);
-  const [sendDevice, { status }] = useMutation(
+  const [sendDevice, { status, error }] = useMutation(
     device ? editDeviceWithId(device.id) : addNewDevice,
     {
       onSuccess: () => {
@@ -47,43 +49,25 @@ export const useNewDeviceForm: (
       },
     },
   );
+  console.log('Error', error);
 
   const onSubmit = async () => {
-    let values: Record<string, any> = {};
     try {
-      values = await form.validateFields();
-    } catch {
-      return;
-    }
+      const values = await form.validateFields();
 
-    console.log('Values', values);
-
-    await sendDevice({
-      name: values.name,
-      regulatorId: values.regulatorId,
-      publicKey: values.publicKey && (await encodeInBase64(values.publicKey)),
-    });
+      console.log('Values', values);
+      await sendDevice({
+        name: values.name,
+        regulatorId: values.regulatorId,
+        publicKey: values.publicKey && (await encodeInBase64(values.publicKey)),
+      });
+    } catch {}
   };
-
-  const regulators: RegulatorModel[] = [
-    {
-      name: 'Regulator 1',
-      id: '11',
-      status: Status.ACTIVE,
-      type: DeviceType.TEMPERATURE,
-    },
-    {
-      name: 'Regulator 3',
-      id: '14',
-      status: Status.INACTIVE,
-      type: DeviceType.LIGHT,
-    },
-  ];
 
   return {
     form,
     onSubmit,
-    regulators,
+    regulators: regulators || [],
     initialValues,
     loading: status === 'loading',
   };
