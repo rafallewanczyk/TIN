d7d39b93c2a88532cc89da2de70611c96988e70f
kejbi <biegajski.kacper@gmail.com>
2020-05-12 21:17:54 +0200

    Fixed header

--- server/src/main/java/pl/kejbi/tin/socket/Protocol.java
+++ server/src/main/java/pl/kejbi/tin/socket/Protocol.java
@@ -2,6 +2,7 @@ package pl.kejbi.tin.socket;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.nio.ByteBuffer;
 
 public interface Protocol {
     int DEVICE_ID = 10001;
@@ -10,9 +11,9 @@ public interface Protocol {
 
     static byte[] messageWithHeader(byte[] dataByteArray) throws IOException {
         ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
-        byteStream.write(PROTOCOL_VERSION);
-        byteStream.write(dataByteArray.length + HEADER_SIZE);
-        byteStream.write(DEVICE_ID);
+        byteStream.write(ByteBuffer.allocate(4).putInt(PROTOCOL_VERSION).array());
+        byteStream.write(ByteBuffer.allocate(4).putInt(dataByteArray.length + HEADER_SIZE).array());
+        byteStream.write(ByteBuffer.allocate(4).putInt(DEVICE_ID).array());
         byteStream.write(dataByteArray);
 
         return byteStream.toByteArray();
