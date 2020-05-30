37a739898e8f8a391c9fe8b12e3a7bc540b3c493
rafallewanczyk <rafallewanczyk@gmail.com>
2020-05-30 17:38:40 +0200

    added tsignore

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
-
+  // @ts-ignore
 if (ReactDOM.createRoot) {
+  // @ts-ignore
   ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<App />);
 } else {
   // @ts-ignore
