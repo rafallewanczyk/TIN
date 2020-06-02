145c95ea96fe757ba5b51edf5662a0f423e79ee6
kostkabrukowa <jiirra@gmail.com>
2020-06-02 23:45:12 +0200

    Adds port configuration for temperature device

--- temerature-device/main.cpp
+++ temerature-device/main.cpp
@@ -8,12 +8,16 @@
 using namespace std;
 
 int main(int argc, char *argv[]) {
+    std::string port;
+    std::cout << "Please give a port number: " ;
+    std::cin >> port;
+
     auto device = std::make_shared<Device>();
     auto deviceHandler = DeviceHandler(device);
 
     std::thread handlerThread(std::ref(deviceHandler));
 
-    Server("20001").run(ConnectionHandler::getConnectionHandler, device);
+    Server(port).run(ConnectionHandler::getConnectionHandler, device);
 
 
     return 0;
